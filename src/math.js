fcc4970b24ef4bad5e37c9acd8a3842a0f00db3d
quemet <129742514+quemet@users.noreply.github.com>
2024-02-19 17:33:48 +0100

    Update math.js

--- src/math.js
+++ src/math.js
@@ -5,7 +5,3 @@ export const soustract = (a, b) => a - b;
 export const multiply = (a, b) => a * b;
 
 export const divide = (a, b) => a / b;
-
-export const power = a => a * a;
-
-export const square = a => Math.sqrt(a);
