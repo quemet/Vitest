f1fca3a7ef897ad188278b7327c430d876977581
quemet <129742514+quemet@users.noreply.github.com>
2024-02-19 17:20:54 +0100

    Update math.test.js

--- test/math.test.js
+++ test/math.test.js
@@ -1,16 +1,12 @@
 import { add, soustract, multiply, divide } from "../src/math";
 import { test, expect } from "vitest";
 
-const max = 1000;
-
-for (let i = 0; i < max; i++) {
-  let a = Math.random() * max;
-  let b = Math.random() * max;
-  let res = a + b;
-  test(`Adds ${a} + ${b} equals ${res}`, () => {
-    expect(add(a, b)).toBe(res);
-  });
-}
+let a = Math.random() * max;
+let b = Math.random() * max;
+let res = a + b;
+test(`Adds ${a} + ${b} equals ${res}`, () => {
+  expect(add(a, b)).toBe(res);
+});
 
 for (let i = 0; i < max; i++) {
   let a = Math.random() * max;
